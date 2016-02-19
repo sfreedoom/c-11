38fd7afd6c79bb5075fdb127ee4ecfa5d65d1787
KIRA <1033956371@qq.com>
2016-02-19 15:25:41 +0800

    [task] add template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include "template.h"
+
+//#ifdef UNIT_TEST
+
+void Validator::Validate(int x, int y, DefenderTest& d){
+
+}
+
+void Validator::Validate(int x, int y, AttackerTest& d){
+
+}
+
+//#endif //UNIT_TEST
